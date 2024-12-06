0971d5706d48199b2d299e10150b1b10b2c5a84a
Cristian Omar <cristianomar71@outlook.com>
2024-12-05 22:29:45 -0600

    Carga final de codigo

---
+++ app/_layout.tsx
@@ -0,0 +1,32 @@
+import { View, Text, Platform } from 'react-native'
+import React from 'react'
+import { Slot } from 'expo-router'
+import { useFonts } from 'expo-font';
+import { StatusBar } from 'expo-status-bar'
+import { globalStyles } from '@/styles/global-styles';
+import * as NavigationBar from 'expo-navigation-bar';
+
+const isAndroid = Platform.OS === 'android';
+
+if (isAndroid) {
+  NavigationBar.setBackgroundColorAsync('black');
+}
+
+const RootLayout = () => {
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <View style={globalStyles.background}>
+      <Slot />
+
+      <StatusBar style="light" />
+    </View>
+  );
+};
+export default RootLayout
---
+++ constants/Colors.ts
@@ -0,0 +1,13 @@
+
+
+export const Colors = {
+
+    darkGray: '#2D2D2D',
+    lightGray: '#9B9B9B',
+    orange: '#FF9427',
+
+    textPrimary: 'white',
+    txteSecondary: '#666666',
+    background: '#000000'
+
+} as const;
